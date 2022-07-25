7b9263a0156d26ff08737e0df8e738b96cca81d7
himanshudevrani96 <himanshu@rapidinnovation.dev>
2022-07-25 19:28:32 +0530

    feat(add-liquidity): implemented-adding-liquidity

--- src/modules/exchange/components/addLiquidity/hook/useAddLiquidity.ts
+++ src/modules/exchange/components/addLiquidity/hook/useAddLiquidity.ts
@@ -1,10 +1,19 @@
 import BigNumber from "bignumber.js";
+import { usePrivateInstances } from "../../../../../blockchain/privateInstance/instances";
 import { PAIR_PUBLIC, ROUTER } from "../../../../../blockchain/publicInstance/instance";
+import { ROUTER_ADDRESS } from "../../../../../blockchain/publicInstance/router";
 
 
 export const useAddLiquidity = () => {
+    const { BUSD, BUST } = usePrivateInstances()
 
-
+    const checkAllowance = async (admin: string, swapType: string) => {
+        try {
+            return await (swapType == "BUSD" ? BUSD : BUST).methods.allowance(admin, ROUTER_ADDRESS).call()
+        } catch (err) {
+            console.error("error alowance", err);
+        }
+    }
     const getBUSTAmount = async (token0: string) => {
         try {
             if (Number(token0)) {
@@ -31,9 +40,76 @@ export const useAddLiquidity = () => {
         }
     }
 
+    const approve = async (account: string, tokenType: string) => {
+        try {
+            const maxAllowance = new BigNumber(2).pow(256).minus(1);
+            (tokenType == "BUSD" ? BUSD : BUST).methods.approve(ROUTER_ADDRESS, maxAllowance).send({
+                from: account
+            }).on("transactionHash", (hash: any) => {
+                alert(hash)
+            }).on("receipt", (receipt: any) => {
+                alert("approve token0 successfull")
+            }).on("error", (error: any, receipt: any) => {
+                alert("approve token0 failed")
+            })
+
+        } catch (err) {
+            console.error("approveToken0", err)
+        }
+    }
+
+    const addLiquidity = async (account: string , token0: string | any, token1: string | any, token0Address: string, token1Address: string, deadLineValue: string, slippage: string) => {
+        try {
+
+            let checkAllowance0: any = checkAllowance(account, "BUSD")
+            checkAllowance0 = new BigNumber(checkAllowance0).dividedBy(10 ** 18)
+            let checkAllowance1: any = checkAllowance(account, "BUST")
+            checkAllowance1 = new BigNumber(checkAllowance1).dividedBy(10 ** 18)
+
+            if (Number(checkAllowance0) < Number(token0) && Number(checkAllowance1) < Number(token1)) {
+                await approve(account, "BUSD")
+                await approve(account, "BUST")
+            } else if (Number(checkAllowance0) < Number(token0)) {
+                await approve(account, "BUSD")
+            } else if (Number(checkAllowance1) < Number(token1)) {
+                await approve(account, "BUST")
+            }
+
+            const deadLine = (Math.round(new Date().getTime() / 1000) + (Number(deadLineValue) ? Number(deadLineValue) * 60 : 5 * 60)).toString()
+            const token0_wei = new BigNumber(token0).times(10 ** 18)
+            const token1_wei = new BigNumber(token1).times(10 ** 18)
+            const minToken0 = Number(token0) - (Number(token0) * (Number(slippage) / 100))
+            const minToken1 = Number(token1) - (Number(token1) * (Number(slippage) / 100))
+            const minToken0_wei = new BigNumber(minToken0).times(10 ** 18)
+            const minToken1_wei = new BigNumber(minToken1).times(10 ** 18)
+
+            ROUTER.methods.addLiquidity(
+                token0Address,
+                token1Address,
+                token0_wei.toFixed(0),
+                token1_wei.toFixed(0),
+                minToken0_wei.toFixed(0),
+                minToken1_wei.toFixed(0),
+                account,
+                deadLine
+            ).send({
+                from: account
+            }).on("transactionHash", (hash: any) => {
+                alert(hash)
+            }).on("receipt", (receipt: any) => {
+                alert("liquidity added successfully")
+            }).on("error", (error: any, receipt: any) => {
+                alert("failed")
+            })
+        } catch (err) {
+            console.error("addLiquidity", err);
+        }
+    }
+
     return {
         getBUSTAmount,
-        getBUSDAmount
+        getBUSDAmount,
+        addLiquidity
     }
 
 }
